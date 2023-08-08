db5feb1a8f66fa26c2874d225a69486a2bbf5956
Alex Li <lialex5883@gmail.com>
2023-08-07 23:20:36 -0400

    added responsive design in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,7 @@ export default function Home({ data }) {
         transition={fadeIn.transition}
         exit={fadeIn.exit}
       >
-        <div className="flex flex-wrap-reverse items-center justify-center mx-5">
+        <div className="flex flex-wrap-reverse items-center justify-center mx-5 overflow-hidden">
           <div className="flex-col">
             <PortfolioSVG />
             <div className="mt-6 max-w-3xl">
@@ -48,51 +48,55 @@ export default function Home({ data }) {
             </M.div>
           </M.div>
         </div>
-        <div className="flex flex-row justify-center my-12 md:mt-6">
-          <M.a
-            href="https://www.linkedin.com/in/zishu-alex-li-54b35718b/"
-            target="_blank"
-            rel="noreferrer"
-            initial={fadeIn.icons.initial}
-            animate={fadeIn.icons.animate}
-            transition={{ ...fadeIn.icons.transition, delay: 1 }}
-            className="mx-7 p-3 rounded-full border-2 border-primary-200 dark:border-primary-dark-200 hover:border-secondary-200 dark:hover:border-secondary-dark-400"
-          >
-            <FaLinkedin className="text-typography dark:text-typography-dark text-3xl transform transition duration-200 hover:scale-110" />
-          </M.a>
-          <M.a
-            href="https://github.com/LiAlex-CS"
-            target="_blank"
-            rel="noreferrer"
-            initial={fadeIn.icons.initial}
-            animate={fadeIn.icons.animate}
-            transition={{ ...fadeIn.icons.transition, delay: 1.3 }}
-            className="mx-7 p-3 rounded-full border-2 border-primary-200 dark:border-primary-dark-200 hover:border-secondary-200 dark:hover:border-secondary-dark-400"
-          >
-            <FaGithub className="text-typography dark:text-typography-dark text-3xl transform transition duration-200 hover:scale-110" />
-          </M.a>
-          <M.a
-            href="https://www.instagram.com/li__alexx/"
-            target="_blank"
-            rel="noreferrer"
-            initial={fadeIn.icons.initial}
-            animate={fadeIn.icons.animate}
-            transition={{ ...fadeIn.icons.transition, delay: 1.6 }}
-            className="mx-7 p-3 rounded-full border-2 border-primary-200 dark:border-primary-dark-200 hover:border-secondary-200 dark:hover:border-secondary-dark-400"
-          >
-            <FaInstagram className="text-typography dark:text-typography-dark text-3xl transform transition duration-200 hover:scale-110" />
-          </M.a>
-          <M.a
-            href={resumeUrl}
-            target="_blank"
-            rel="noreferrer"
-            initial={fadeIn.icons.initial}
-            animate={fadeIn.icons.animate}
-            transition={{ ...fadeIn.icons.transition, delay: 1.9 }}
-            className="mx-7 p-3 rounded-full border-2 border-primary-200 dark:border-primary-dark-200 hover:border-secondary-200 dark:hover:border-secondary-dark-400"
-          >
-            <HiOutlineDocumentText className="text-typography dark:text-typography-dark text-3xl transform transition duration-200 hover:scale-110" />
-          </M.a>
+        <div className="flex flex-row flex-wrap justify-center">
+          <div className="flex flex-row my-12 md:mt-6">
+            <M.a
+              href="https://www.linkedin.com/in/zishu-alex-li-54b35718b/"
+              target="_blank"
+              rel="noreferrer"
+              initial={fadeIn.icons.initial}
+              animate={fadeIn.icons.animate}
+              transition={{ ...fadeIn.icons.transition, delay: 1 }}
+              className="mx-7 p-3 rounded-full border-2 border-primary-200 dark:border-primary-dark-200 hover:border-secondary-200 dark:hover:border-secondary-dark-400"
+            >
+              <FaLinkedin className="text-typography dark:text-typography-dark text-3xl transform transition duration-200 hover:scale-110" />
+            </M.a>
+            <M.a
+              href="https://github.com/LiAlex-CS"
+              target="_blank"
+              rel="noreferrer"
+              initial={fadeIn.icons.initial}
+              animate={fadeIn.icons.animate}
+              transition={{ ...fadeIn.icons.transition, delay: 1.3 }}
+              className="mx-7 p-3 rounded-full border-2 border-primary-200 dark:border-primary-dark-200 hover:border-secondary-200 dark:hover:border-secondary-dark-400"
+            >
+              <FaGithub className="text-typography dark:text-typography-dark text-3xl transform transition duration-200 hover:scale-110" />
+            </M.a>
+          </div>
+          <div className="flex flex-row my-12 md:mt-6">
+            <M.a
+              href="https://www.instagram.com/li__alexx/"
+              target="_blank"
+              rel="noreferrer"
+              initial={fadeIn.icons.initial}
+              animate={fadeIn.icons.animate}
+              transition={{ ...fadeIn.icons.transition, delay: 1.6 }}
+              className="mx-7 p-3 rounded-full border-2 border-primary-200 dark:border-primary-dark-200 hover:border-secondary-200 dark:hover:border-secondary-dark-400"
+            >
+              <FaInstagram className="text-typography dark:text-typography-dark text-3xl transform transition duration-200 hover:scale-110" />
+            </M.a>
+            <M.a
+              href={resumeUrl}
+              target="_blank"
+              rel="noreferrer"
+              initial={fadeIn.icons.initial}
+              animate={fadeIn.icons.animate}
+              transition={{ ...fadeIn.icons.transition, delay: 1.9 }}
+              className="mx-7 p-3 rounded-full border-2 border-primary-200 dark:border-primary-dark-200 hover:border-secondary-200 dark:hover:border-secondary-dark-400"
+            >
+              <HiOutlineDocumentText className="text-typography dark:text-typography-dark text-3xl transform transition duration-200 hover:scale-110" />
+            </M.a>
+          </div>
         </div>
       </M.div>
     </Layout>
