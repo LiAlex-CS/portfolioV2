845f28ec27cfe67f0f7f1b85f3e3d56541431f90
Alex Li <lialex5883@gmail.com>
2023-08-12 00:39:59 -0400

    setup useEmail hook

--- src/services/emailJs/useEmail.js
+++ src/services/emailJs/useEmail.js
@@ -2,24 +2,22 @@ import { useState, useEffect } from "react";
 import emailjs from "@emailjs/browser";
 
 export const useSendEmail = (options) => {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
   useEffect(() => emailjs.init(process.env.EMAIL_PUBLIC_KEY), []);
+  const [sendingEmailLoading, setSendingEmailLoading] = useState(false);
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+  const handleSubmit = async () => {
     const SERVICE_ID = "portfolio_email_service";
     const TEMPLATE_ID = "portfolio_contact_email";
 
     try {
-      setLoading(true);
-      await emailjs.send(SERVICE_ID, TEMPLATE_ID, options);
+      setSendingEmailLoading(true);
+      return await emailjs.send(SERVICE_ID, TEMPLATE_ID, options);
     } catch (error) {
-      setError(error);
+      throw error;
     } finally {
-      setLoading(false);
+      setSendingEmailLoading(false);
     }
   };
 
-  return [loading, error, handleSubmit];
+  return [sendingEmailLoading, handleSubmit];
 };
