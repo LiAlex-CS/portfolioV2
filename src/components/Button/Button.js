456aac5ba727f7e69bd433f65221a20ba843c8e6
Alex Li <lialex5883@gmail.com>
2023-08-12 21:29:26 -0400

    added disabled attr for button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,13 +2,13 @@ import React from "react";
 import { PLarge } from "../Typography/Text";
 import { CgSpinner } from "react-icons/cg";
 
-const Button = ({ label, onClick, type, loading }) => {
+const Button = ({ label, onClick, type, loading, disabled }) => {
   return (
     <button
-      className="bg-secondary-500 dark:bg-secondary-dark-500 hover:bg-secondary-600 dark:hover:bg-secondary-dark-600 active:bg-secondary-800 dark:active:bg-secondary-dark-800 rounded-lg px-5 py-2.5 m-4"
+      className="bg-secondary-500 dark:bg-secondary-dark-500 hover:bg-secondary-600 dark:hover:bg-secondary-dark-600 active:bg-secondary-800 dark:active:bg-secondary-dark-800 rounded-lg px-5 py-2.5 m-4 disabled:bg-secondary-dark-300 dark:disabled:bg-secondary-dark-300 disabled:cursor-not-allowed"
       onClick={onClick}
       type={type}
-      disabled={loading}
+      disabled={loading || disabled}
     >
       {loading ? (
         <CgSpinner
