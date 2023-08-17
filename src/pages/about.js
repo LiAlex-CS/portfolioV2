e14f281c49436738dc037e2a8e873856c4586bfd
Alex Li <lialex5883@gmail.com>
2023-08-17 02:06:01 -0400

    added About me page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,21 +1,114 @@
 import React from "react";
 import Layout from "../components/Layout";
-import { H1 } from "../components/Typography/Text";
+import { PLarge, Title, H2, P } from "../components/Typography/Text";
 import { motion as M } from "framer-motion";
 import { fadeIn } from "../styles/animations";
+import { graphql } from "gatsby";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import { twMerge } from "tailwind-merge";
 
-export default function About() {
+const AboutMeSection = ({ header, body, imageData, caption, className }) => {
+  const isIntro = header === "Introduction";
+
+  return (
+    <div
+      className={twMerge(
+        "flex flex-col justify-center items-center",
+        className
+      )}
+    >
+      <div
+        className={`rounded-3xl flex flex-row flex-wrap text-start items-center my-8 w-11/12 lg:w-3/4 2xl:w-1/2 p-8 ${
+          isIntro ? "" : "bg-primary-400 dark:bg-primary-dark-200"
+        }`}
+      >
+        {!isIntro ? <H2 className="mb-10">{header}</H2> : null}
+        <PLarge>{body}</PLarge>
+      </div>
+      {imageData ? (
+        <div className="rounded-3xl flex flex-col my-10 bg-primary-400 dark:bg-primary-dark-200 max-w-2xl mx-7">
+          <GatsbyImage
+            image={getImage(imageData)}
+            alt={imageData.description}
+            className="rounded-t-3xl"
+          />
+          <P className="m-4 text-gray-400 dark:text-zinc-400">{caption}</P>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default function About({ data }) {
+  const aboutMeData = data.allContentfulAboutMeSection.nodes;
+  const sortedAboutMeData = aboutMeData.sort(
+    (sectionA, sectionB) => sectionA.order - sectionB.order
+  );
+
   return (
     <Layout>
       <M.div
-        className="flex flex-col w-full pt-16"
+        className="flex flex-col w-full pt-16 text-center items-center"
         initial={fadeIn.initial}
         animate={fadeIn.animate}
         transition={fadeIn.transition}
         exit={fadeIn.exit}
       >
-        <H1>About</H1>
+        <Title className="my-16 lg:mt-28 xl:mt-36">A Little About Me</Title>
+        {sortedAboutMeData.map((section) => {
+          return (
+            <AboutMeSection
+              key={section.contentful_id}
+              header={section.header}
+              body={section.body.body}
+              imageData={section.image}
+              caption={section.imageCaption}
+            />
+          );
+        })}
+        {/* <div className="rounded-3xl bg-primary-400 dark:bg-primary-dark-200 flex flex-row flex-wrap text-start items-center my-8 w-11/12 lg:w-3/4 2xl:w-1/2 p-8">
+          <H2 className="mb-10">My Education</H2>
+          <PLarge>
+            Lorem ipsum dolor sit amet consectetur. Auctor ultricies at quis
+            pellentesque euismod eget gravida. Vel nunc odio ut venenatis
+            faucibus blandit sed enim lectus. Orci gravida ipsum sem diam
+            facilisi varius. Augue nam orci nisl mauris. Pretium volutpat justo
+            varius ornare porta. Sed sit id id quam. Id et vel amet tempor sit
+            augue nisl aliquam neque.
+          </PLarge>
+        </div>
+        <div className="rounded-3xl bg-primary-400 dark:bg-primary-dark-200 flex flex-row flex-wrap text-start items-center my-8 w-11/12 lg:w-3/4 2xl:w-1/2 p-8">
+          <H2 className="mb-10">Personal Hobbies & Recreation</H2>
+          <PLarge>
+            Lorem ipsum dolor sit amet consectetur. Auctor ultricies at quis
+            pellentesque euismod eget gravida. Vel nunc odio ut venenatis
+            faucibus blandit sed enim lectus. Orci gravida ipsum sem diam
+            facilisi varius. Augue nam orci nisl mauris. Pretium volutpat justo
+            varius ornare porta. Sed sit id id quam. Id et vel amet tempor sit
+            augue nisl aliquam neque.
+          </PLarge>
+        </div> */}
       </M.div>
     </Layout>
   );
 }
+
+export const query = graphql`
+  query AboutMeQuery {
+    allContentfulAboutMeSection {
+      nodes {
+        contentful_id
+        header
+        body {
+          body
+        }
+        image {
+          gatsbyImageData(layout: FULL_WIDTH)
+          description
+        }
+        imageCaption
+        order
+      }
+    }
+  }
+`;
